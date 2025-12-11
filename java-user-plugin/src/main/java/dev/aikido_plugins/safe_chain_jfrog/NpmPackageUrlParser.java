a6216b42308f4fad42fe6473cac05658d3305e1b
BitterPanda <bitterpanda@proton.me>
2025-12-11 19:39:37 +0100

    Create NpmPackageUrlParser and its test cases

---
+++ java-user-plugin/src/main/java/dev/aikido_plugins/safe_chain_jfrog/NpmPackageUrlParser.java
@@ -0,0 +1,38 @@
+package dev.aikido_plugins.safe_chain_jfrog;
+
+public class NpmPackageUrlParser {
+
+  public record ParsedNpmPackage(String packageName, String version) {}
+
+  public static ParsedNpmPackage parseNpmPackageUrl(String urlPath) {
+    String packageName = null;
+    String version = null;
+
+    int separatorIndex = urlPath.indexOf("/-/");
+    if (separatorIndex == -1) {
+      return new ParsedNpmPackage(packageName, version);
+    }
+
+    packageName = urlPath.substring(0, separatorIndex);
+    String filename = urlPath.substring(
+      separatorIndex + 3,
+      urlPath.length() - 4
+    ); // Remove /-/ and .tgz
+
+    // Extract version from filename
+    if (packageName.startsWith("@")) {
+      String scopedPackageName = packageName.substring(
+        packageName.lastIndexOf("/") + 1
+      );
+      if (filename.startsWith(scopedPackageName + "-")) {
+        version = filename.substring(scopedPackageName.length() + 1);
+      }
+    } else {
+      if (filename.startsWith(packageName + "-")) {
+        version = filename.substring(packageName.length() + 1);
+      }
+    }
+
+    return new ParsedNpmPackage(packageName, version);
+  }
+}
---
+++ java-user-plugin/src/test/java/NpmPackageUrlParserTest.java
@@ -0,0 +1,62 @@
+import dev.aikido_plugins.safe_chain_jfrog.NpmPackageUrlParser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NpmPackageUrlParserTest {
+
+  @Test
+  public void testParseScopedPackage() {
+    String urlPath = "@babel/core/-/core-7.21.4.tgz";
+    NpmPackageUrlParser.ParsedNpmPackage result = NpmPackageUrlParser.parseNpmPackageUrl(urlPath);
+    assertEquals("@babel/core", result.packageName());
+    assertEquals("7.21.4", result.version());
+  }
+
+  @Test
+  public void testParseRegularPackage() {
+    String urlPath = "lodash/-/lodash-4.17.21.tgz";
+    NpmPackageUrlParser.ParsedNpmPackage result = NpmPackageUrlParser.parseNpmPackageUrl(urlPath);
+    assertEquals("lodash", result.packageName());
+    assertEquals("4.17.21", result.version());
+  }
+
+  @Test
+  public void testParseInvalidUrlNoTgz() {
+    String urlPath = "lodash/-/lodash-4.17.21";
+    NpmPackageUrlParser.ParsedNpmPackage result = NpmPackageUrlParser.parseNpmPackageUrl(urlPath);
+    assertNull(result.packageName());
+    assertNull(result.version());
+  }
+
+  @Test
+  public void testParseInvalidUrlNoSeparator() {
+    String urlPath = "lodash-4.17.21.tgz";
+    NpmPackageUrlParser.ParsedNpmPackage result = NpmPackageUrlParser.parseNpmPackageUrl(urlPath);
+    assertNull(result.packageName());
+    assertNull(result.version());
+  }
+
+  @Test
+  public void testParseEmptyUrl() {
+    String urlPath = "";
+    NpmPackageUrlParser.ParsedNpmPackage result = NpmPackageUrlParser.parseNpmPackageUrl(urlPath);
+    assertNull(result.packageName());
+    assertNull(result.version());
+  }
+
+  @Test
+  public void testParseScopedPackageWithComplexName() {
+    String urlPath = "@angular/animations/-/animations-15.2.0.tgz";
+    NpmPackageUrlParser.ParsedNpmPackage result = NpmPackageUrlParser.parseNpmPackageUrl(urlPath);
+    assertEquals("@angular/animations", result.packageName());
+    assertEquals("15.2.0", result.version());
+  }
+
+  @Test
+  public void testParseRegularPackageWithComplexName() {
+    String urlPath = "react-dom/-/react-dom-18.2.0.tgz";
+    NpmPackageUrlParser.ParsedNpmPackage result = NpmPackageUrlParser.parseNpmPackageUrl(urlPath);
+    assertEquals("react-dom", result.packageName());
+    assertEquals("18.2.0", result.version());
+  }
+}
